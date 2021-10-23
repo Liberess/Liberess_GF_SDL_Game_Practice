ddb1ca5adc945f4487372708b0ae67dacea43ce2
Liberess <58577796+Liberess@users.noreply.github.com>
2021-10-23 12:07:45 +0000

    실습1~3

--- main.cpp
+++ main.cpp
@@ -5,31 +5,31 @@ SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[])
 {
- 
-   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+   if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
    {
+      g_pWindow = SDL_CreateWindow
+        (
+          "HelloSDL", // 실습1
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED,
+          512, 512, // 실습2
+          SDL_WINDOW_SHOWN
+        );
 
-        g_pWindow = SDL_CreateWindow("Setting up SDL 10/22",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            640, 480, SDL_WINDOW_SHOWN);
-
-        if (g_pWindow != 0)
-        {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-
+      if (g_pWindow != 0)
+        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
     }
-    else {
-        return 1;
+    else
+    {
+      return 1;
     }
 
-    SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
-    SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255); // 실습3
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
 
-    SDL_Delay(5000);
-    SDL_Quit();
+  SDL_Delay(5000);
+  SDL_Quit();
 
-    return 0;
+  return 0;
 }
