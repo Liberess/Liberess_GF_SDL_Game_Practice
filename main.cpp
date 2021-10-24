6c5831f79b4dbad6338da7bda98a98111b00642b
Liberess <58577796+Liberess@users.noreply.github.com>
2021-10-24 01:43:41 +0000

    HelloSDL분리하기

--- main.cpp
+++ main.cpp
@@ -2,34 +2,74 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
+
+int main(int argc, char* args[]); // 실습1
+bool Init(const char* title, int xPos, int yPos, int width, int height, int flags);
+void Update();
+void Render();
 
 int main(int argc, char* args[])
 {
-   if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-   {
-      g_pWindow = SDL_CreateWindow
-        (
-          "HelloSDL", // 실습1
-          SDL_WINDOWPOS_CENTERED,
-          SDL_WINDOWPOS_CENTERED,
-          512, 512, // 실습2
-          SDL_WINDOW_SHOWN
-        );
+  if(Init
+      (
+        "Breaking Up HelloSDL",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN)
+      )
+  {
+    g_bRunning = true;
+  }
+  else
+  {
+    return 1;
+  }
 
-      if (g_pWindow != 0)
-        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-    }
-    else
-    {
-      return 1;
-    }
+  while(g_bRunning)
+  {
+    Update(); // 실습2
+    Render();
+  }
 
-  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255); // 실습3
-  SDL_RenderClear(g_pRenderer);
-  SDL_RenderPresent(g_pRenderer);
-
-  SDL_Delay(5000);
   SDL_Quit();
 
   return 0;
+}
+
+bool Init(const char* title, int xPos, int yPos, int width, int height, int flags)
+{
+  if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    return false;
+
+  g_pWindow = SDL_CreateWindow(title, xPos, yPos, width, height, flags);
+
+  if (g_pWindow != 0)
+    g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 0);
+
+  return true;
+}
+
+void Update()
+{
+  static int count = 0;
+
+  if(count >= 5)
+  {
+    g_bRunning = false;
+    return;
+  }
+
+  SDL_SetRenderDrawColor(g_pRenderer, rand() % 256, rand() % 256, rand() % 256, 255);
+  SDL_Delay(1000);
+  ++count;
+}
+
+void Render()
+{
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
 }
