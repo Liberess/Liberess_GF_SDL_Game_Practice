56468020969187253b10091eae645ab3bd04729e
Liberess <58577796+Liberess@users.noreply.github.com>
2021-10-28 13:20:23 +0000

    HelloSDL분리

--- main.cpp
+++ main.cpp
@@ -2,31 +2,76 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false; //실습1. 게임 실행 yes/no
+
+int main(int argc, char* args[]); //실습2
+bool Init(const char* title, int x, int y, int h, int w, int flags);
+void Update();
+void Render();
 
 int main(int argc, char* args[])
 {
-   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-   {
+  if(Init("Breaking Up HelloSDL",
+          SDL_WINDOWPOS_CENTERED,
+          SDL_WINDOWPOS_CENTERED,
+          640, 480,
+          SDL_WINDOW_SHOWN))
+  {
+    g_bRunning = true;
+  }
+  else
+  {
+    return 1;
+  }
 
-        g_pWindow = SDL_CreateWindow("HelloSDL", //실습1
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            512, 512, SDL_WINDOW_SHOWN); //실습2
+  while(g_bRunning)
+  {
+    Update(); //실습3
+    Render();
+  }
 
-        if (g_pWindow != 0)
-          g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+  SDL_Quit();
 
-    }
-    else {
-        return 1;
-    }
+  return 0;
+}
 
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255); //실습3
-    SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
+bool Init(const char* title, int x, int y, int h, int w, int flags)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+  {
+    g_pWindow = SDL_CreateWindow(title, x, y, w, h, flags); 
 
-    SDL_Delay(5000);
-    SDL_Quit();
+    if (g_pWindow != 0)
+      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 
-    return 0;
+  }
+  else
+  {
+    return false;
+  }
+
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255); 
+
+  return true;
+}
+
+void Update()
+{
+  static int count = 0;
+
+  if(count >= 3)
+  {
+    g_bRunning = false; //실습4
+    return;
+  }
+
+  ++count;
+
+  SDL_Delay(1000);
+}
+
+void Render()
+{
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
 }
